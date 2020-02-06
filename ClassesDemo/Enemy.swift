cdb4291ccb1056e0dbf1f244da8984f4620af8b5
Adriano <adriano.kaito@gmail.com>
2020-02-06 20:06:43 -0300

    commit

---
+++ ClassesDemo/Enemy.swift
@@ -0,0 +1,22 @@
+
+class Enemy {
+    var health: Int
+    var attackStrength: Int
+    
+    init(health: Int, attackStrength: Int) {
+        self.health = health
+        self.attackStrength = attackStrength
+    }
+    
+    func takeDamage(amount: Int) {
+        health = health - amount
+    }
+    
+    func move() {
+        print("Walk forwards.")
+    }
+    
+    func attack() {
+        print("Land a hit, does \(attackStrength) damage.")
+    }
+}
---
+++ ClassesDemo/main.swift
@@ -0,0 +1,10 @@
+
+let skeleton = Enemy(health: 100, attackStrength: 10)
+
+
+let dragon = Dragon(health: 100, attackStrength: 20)
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "Hi!")
+dragon.move()
+dragon.attack()
